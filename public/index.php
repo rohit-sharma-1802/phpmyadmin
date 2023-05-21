d3d58a44db3bfa854d9e310b19633005202f25ba
rohit-sharma-1802 <59288117+rohit-sharma-1802@users.noreply.github.com>
2023-05-21 23:32:25 +0530

    Update index.php
    string can be written in single, so why using concatenate

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use PhpMyAdmin\Common;
 
 // phpcs:disable PSR1.Files.SideEffects
-if (! defined('ROOT_PATH')) {
+if (!defined('ROOT_PATH')) {
     define('ROOT_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
 }
 
@@ -20,10 +20,10 @@ require_once ROOT_PATH . 'libraries/constants.php';
 
 if (! @is_readable(AUTOLOAD_FILE)) {
     die(
-        '<p>File <samp>' . AUTOLOAD_FILE . '</samp> missing or not readable.</p>'
-        . '<p>Most likely you did not run Composer to '
-        . '<a href="https://docs.phpmyadmin.net/en/latest/setup.html#installing-from-git">'
-        . 'install library files</a>.</p>'
+        '<p>File <samp>' . AUTOLOAD_FILE . '</samp> missing or not readable.</p>
+        <p>
+            Most likely you did not run Composer to<a href="https://docs.phpmyadmin.net/en/latest/setup.html#installing-from-git">install library files</a>
+        </p>'
     );
 }
 
